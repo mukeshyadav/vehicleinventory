7f27e29177b00adb53741dc22ccdeb079fa4ac3d
mukeshyadav <mukeshyadav@gmail.com>
2020-06-06 22:04:11 +0530

    adding images, layout for vehicle listing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import Header from './components/header'
+import React from "react";
+import Header from "./components/header";
+import IcoFilter from "./images/filter.svg";
+import IcoAdd from "./images/add.svg";
 
-function App () {
+function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
-      <div className='w-full max-w-screen-xl mx-auto px-6'>
-        <div className='grid grid-cols-2 gap-4'>
-          <div className='col-span-2'>
-            Vehicle
+      <div className="flex mb-4 pt-20 px-6 w-full max-w-screen-xl mx-auto">
+        <div className="w-1/4">
+          <div className="flex items-center justify-between border-b pb-3 mb-3">
+            <h2 className="text-lg font-bold">Vehicle</h2>
+            <div className="inline-flex">
+              <button className="bg-transparent hover:bg-gray-200 py-2 px-4 border border-gray-500 rounded-l">
+                <img src={IcoFilter} alt="Apply Filter" className="h-5" />
+              </button>
+              <button className="bg-transparent hover:bg-gray-200 py-2 px-4 border-gray-500 border rounded-r">
+                <img src={IcoAdd} alt="Apply Filter" className="h-5" />
+              </button>
+            </div>
           </div>
-          <div className='col-span-1'>
-            Details
+          <div className="results">
+            <ul>
+              <li className="border-b pb-2 pt-2">Vehicle 1</li>
+              <li className="border-b pb-2 pt-2">Vehicle 1</li>
+            </ul>
           </div>
         </div>
+        <div class="w-3/4"></div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
