8ee6059809860b20b0f458b596de0000687d7203
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-11 21:27:31 +0530

    adding add vehicle component

---
+++ src/components/AddVehicle/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AddVehicle() {
+  return (
+    <form className="w-full max-w-lg">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb:0"></div>
+      </div>
+    </form>
+  );
+}
