44a99cf75103c8d9befece6f9296feca9041b449
Mukesh Yadav <mukeshyadav@gmail.com>
2020-06-28 20:42:34 +0530

    dispatching event with current vehicle clicked id

--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -5,7 +5,7 @@ import { useApi } from "../../hooks/useApi";
 
 export default function VehicleList() {
   const { state, dispatch } = useContext(vehicleContext);
-  const [isFetching, setIsFetching] = useInfniteScroll(fetchMoreListItems);
+  // const [isFetching, setIsFetching] = useInfniteScroll(fetchMoreListItems);
   const [currentPageIndex, setPageIndex] = useState(1);
 
   let allVehiclesData = useApi(
@@ -16,25 +16,31 @@ export default function VehicleList() {
     dispatch({ type: "GET_VEHICLES", payload: allVehiclesData });
   }, [allVehiclesData]);
 
-  function fetchMoreListItems() {
-    fetch(`http://localhost:3001/vehicles?_page=${currentPageIndex}&_limit=5`)
-      .then(res => res.json())
-      .then(resData => {
-        dispatch({ type: "GET_VEHICLES", payload: resData });
-        setPageIndex(currentPageIndex + 1);
-        setIsFetching(false);
-      });
-  }
+  // function fetchMoreListItems() {
+  //   fetch(`http://localhost:3001/vehicles?_page=${currentPageIndex}&_limit=5`)
+  //     .then(res => res.json())
+  //     .then(resData => {
+  //       dispatch({ type: "GET_VEHICLES", payload: resData });
+  //       setPageIndex(currentPageIndex + 1);
+  //       setIsFetching(false);
+  //     });
+  // }
 
   return (
     <div className="results">
       <ul>
         {state.vehicles.map(vehicle => (
-          <li className="border-b pb-2 pt-2" key={vehicle.id}>
+          <li
+            className="border-b pb-2 pt-2"
+            key={vehicle.id}
+            onClick={e =>
+              dispatch({ type: "SET_CURRENT_VEHICLE", payload: vehicle.id })
+            }
+          >
             {vehicle.model} ({vehicle.qty})
           </li>
         ))}
-        {isFetching && "Fetching more vehicles..."}
+        {/* {isFetching && "Fetching more vehicles..."} */}
       </ul>
     </div>
   );
